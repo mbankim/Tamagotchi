c320fa9547db6d144bdcfc083f011adca03e463c
s38qiu <s38qiu@06e43772-ae1b-4c0d-9892-c0dabbf93334>
2016-11-30 05:06:31 +0000

    All implementations have been added (evolution, shop, minigames, taking care of the pet). All that needs to be done is to clean up the code.

--- Tamagotchi/Game_UI.ino
+++ Tamagotchi/Game_UI.ino
@@ -10,18 +10,21 @@
 //Potentiometer: 0 - 4095
 
 //gameUiPages
-const uint32_t MainScreen    = 0;  //Displays Tamagotchi and Information
-const uint32_t ShopMenu      = 1;  //Shop for Food or Gifts
-const uint32_t FoodMenu      = 2;  //Can pick among various Foods
-const uint32_t GiftMenu      = 3;  //Can pick among various Gifts
-const uint32_t GameMenu      = 4;  //Can pick among various Games
-const uint32_t GameOver      = 5;  //Game over screen
-const uint32_t StartScreen      = 6;  //Welcome screen (egg hatching)
+const uint8_t MainScreen    = 0;  //Displays Tamagotchi and Information
+const uint8_t ShopMenu      = 1;  //Shop for Food or Gifts
+const uint8_t FoodMenu      = 2;  //Can pick among various Foods
+const uint8_t GiftMenu      = 3;  //Can pick among various Gifts
+const uint8_t GameMenu      = 4;  //Can pick among various Games
+const uint8_t GameOver      = 5;  //Game over screen
+const uint8_t StartScreen   = 6;  //Welcome screen (egg hatching)
+const uint8_t DanceGame     = 7;  //Dance game
+const uint8_t JumpGame      = 8;  //Jump game
+
 /*
- * GameMenu1 etc
- * How many games will there be?
- */
-const uint32_t NumberOfPages = 5;  //To be increased with number of games.
+   GameMenu1 etc
+   How many games will there be?
+*/
+const uint8_t NumberOfPages = 9;  //To be increased with number of games.
 uint32_t gameUiPage = MainScreen;
 
 int GlobalOption = 0;
@@ -36,46 +39,47 @@ const uint32_t Potentiometer = PE_3;
 const float PotentiometerMaxValue = 4095;
 
 //Approximately 175 loops per second
-const uint32_t secondTick=175;
-const uint32_t hungerLimit=secondTick*60*5; //5 minutes
-const uint32_t happyLimit=secondTick*60*7; //7 minutes
-const uint32_t toiletLimit=secondTick*60*8; //8 minutes
+const uint32_t secondTick = 175;
+const uint32_t hungerLimit = secondTick * 60 * 5; //5 minutes
+const uint32_t happyLimit = secondTick * 60 * 7; //7 minutes
+const uint32_t toiletLimit = secondTick * 60 * 8; //8 minutes
 
 int HUNGER_MIN = 0;
 int HAPPY_MIN = 0;
 const uint32_t TOILET_MAX = 8;
-const uint32_t toiletShakeRequire=50;
+const uint32_t toiletShakeRequire = 50;
 
-const uint32_t foodPrices[6][3]={{100,1,0}, //milk
-                                 {200,2,0}, //bread
-                                 {300,2,1}, //pie
-                                 {400,2,1}, //cherries
-                                 {600,3,2}, //burger
-                                 {200,1,1}}; //drink
-const uint32_t giftPrices[6][2]={{100,1}, //film
-                                 {200,2}, //dumbbells
-                                 {300,1}, //joystick
-                                 {200,3}, //headphones
-                                 {400,2}, //baseball
-                                 {1000,1}}; //evolution
+const uint32_t foodPrices[6][3] = {{100, 1, 0}, //milk
+  {200, 2, 0}, //bread
+  {300, 2, 1}, //pie
+  {400, 2, 1}, //cherries
+  {600, 3, 2}, //burger
+  {200, 1, 1}
+}; //drink
+const uint32_t giftPrices[6][2] = {{100, 1}, //film
+  {200, 2}, //dumbbells
+  {300, 1}, //joystick
+  {200, 3}, //headphones
+  {400, 2}, //baseball
+  {1000, 1}
+}; //evolution
 
 //End game
-bool dead=false;
+bool dead = false;
 
-enum LifeStages
-{
-  Egg = 0,
-  Baby = 1,
-  Teen = 2,
-  Adult = 3,
-  Senior = 4,
-  NumberOfAges = 5,
-};
+
+const uint8_t EGG = 0;
+const uint8_t BABY = 1;
+const uint8_t TEEN = 2;
+const uint8_t ADULT = 3;
+const uint8_t SENIOR = 4;
+
+
 
 typedef struct PlayerStats
 {
   //Age ranges from 0 (egg) to 4 (senior)
-  LifeStages age;
+  int age;
   //Hunger ranges from 0 to 5
   int hunger;
   uint32_t hungerCounter;
@@ -87,7 +91,7 @@ typedef struct PlayerStats
   uint32_t toiletCounter;
   //Money can be obtained from playing games
   uint32_t money;
-}Player;
+} Player;
 Player player;
 #define RED RED_LED
 #define GREEN GREEN_LED
@@ -118,18 +122,18 @@ void GameUIInit()
   OrbitOledSetDrawMode(modOledSet);
 
   gameInputState = { 0 };
-  player = {Egg,5,0,5,0,0,0,0};
+  player = {1, 5, 0, 5, 0, 0, 0, 9999}; //{0,5,0,5,0,0,0,0}
   //activeGame = { 0 };
 
 
   //Set the hardware we're using to INPUT mode
-  for(int i = 0; i < SwitchCount; ++i )
+  for (int i = 0; i < SwitchCount; ++i )
     pinMode(Switches[i], INPUT);
-  for(int i = 0; i < ButtonCount; ++i )
+  for (int i = 0; i < ButtonCount; ++i )
     pinMode(Buttons[i], INPUT);
 
-  pinMode(RED,OUTPUT);
-  pinMode(GREEN,OUTPUT); 
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
 }
 
 void goToPage(int page) {
@@ -138,7 +142,7 @@ void goToPage(int page) {
   gameUiPage = page;
 }
 
-int shakeCounter=0;
+int shakeCounter = 0;
 
 void handleMainScreen() {
   //display health/happy statusd
@@ -148,7 +152,7 @@ void handleMainScreen() {
   DrawMainScreen(player.hunger, player.happy, player.money, player.toilet, player.age);
   OrbitOledUpdate();
 
-  if(dead)
+  if (dead)
     goToPage(GameOver);
   //Serial.println(gameInputState.buttons[0].isRising);
   if (gameInputState.buttons[0].isRising) {
@@ -161,55 +165,55 @@ void handleMainScreen() {
 
 
 
-void StatsTick(){
+void StatsTick() {
   player.hungerCounter++;
   player.happyCounter++;
   player.toiletCounter++;
 
-  if(player.hungerCounter>=hungerLimit){
+  if (player.hungerCounter >= hungerLimit) {
     player.hunger--;
-    if(player.hunger<HUNGER_MIN)
-      dead=true;
-    player.hungerCounter=0;
+    if (player.hunger < HUNGER_MIN)
+      dead = true;
+    player.hungerCounter = 0;
     OrbitOledClearBuffer();
     OrbitOledClear();
   }
-  if(player.happyCounter>=happyLimit){
+  if (player.happyCounter >= happyLimit) {
     player.happy--;
-    if(player.happy<HAPPY_MIN) //player dies when really unhappy...?
-      dead=true;
-    player.happyCounter=0;
+    if (player.happy < HAPPY_MIN) //player dies when really unhappy...?
+      dead = true;
+    player.happyCounter = 0;
     OrbitOledClearBuffer();
     OrbitOledClear();
   }
-  if(player.toiletCounter>=toiletLimit){
+  if (player.toiletCounter >= toiletLimit) {
     player.toilet++;
-    if(player.toilet>TOILET_MAX)
-      dead=true;
-    player.toiletCounter=0;
+    if (player.toilet > TOILET_MAX)
+      dead = true;
+    player.toiletCounter = 0;
     OrbitOledClearBuffer();
     OrbitOledClear();
   }
 
-  
-  if(ShakeIsShaking()){
-        shakeCounter++;
+
+  if (ShakeIsShaking()) {
+    shakeCounter++;
   }
-    if(shakeCounter>=toiletShakeRequire)
-    {
-      OrbitOledClearBuffer();
-      OrbitOledClear();
-      player.toilet=0;
-      Serial.println(player.toilet);
-      shakeCounter=0;
-    }
-  
+  if (shakeCounter >= toiletShakeRequire)
+  {
+    OrbitOledClearBuffer();
+    OrbitOledClear();
+    player.toilet = 0;
+    Serial.println(player.toilet);
+    shakeCounter = 0;
+  }
+
 }
 
 
-void handleShopMenu(){
-  int x=11, y1=5, y2=18, offset=9; //where am I putting this
-  int option = gameInputState.potentiometer/(PotentiometerMaxValue/2+1);
+void handleShopMenu() {
+  int x = 11, y1 = 5, y2 = 18, offset = 9; //where am I putting this
+  int option = gameInputState.potentiometer / (PotentiometerMaxValue / 2 + 1);
   if (option != GlobalOption) {
     OrbitOledClear();
     GlobalOption = option;
@@ -222,7 +226,7 @@ void handleShopMenu(){
   OrbitOledDrawString("Gifts");
 
   //display the arrow
-  OrbitOledMoveTo(x-offset, (option == 0 ? y1 : y2));
+  OrbitOledMoveTo(x - offset, (option == 0 ? y1 : y2));
   OrbitOledDrawChar('>');
 
   if (gameInputState.buttons[0].isRising && option == 0) {
@@ -238,22 +242,22 @@ void handleShopMenu(){
 }
 
 
-int eatFood(int value){
-  player.hunger+=value;
-  if(player.hunger>5)
-    player.hunger=5;
+int eatFood(int value) {
+  player.hunger += value;
+  if (player.hunger > 5)
+    player.hunger = 5;
 }
 
-int gainHappy(int value){
-  player.happy+=value;
-  if(player.happy>5)
-    player.happy=5;
+int gainHappy(int value) {
+  player.happy += value;
+  if (player.happy > 5)
+    player.happy = 5;
 }
 
 void handleFoodMenu() {
   //display food options
   //display blablabla
-  int option = gameInputState.potentiometer/(PotentiometerMaxValue/6 + 1.0);
+  int option = gameInputState.potentiometer / (PotentiometerMaxValue / 6 + 1.0);
   if (option != GlobalOption) {
     OrbitOledClear();
     GlobalOption = option;
@@ -261,15 +265,16 @@ void handleFoodMenu() {
   displayFood(option, foodPrices[option][0], foodPrices[option][1], foodPrices[option][2]);
   if (gameInputState.buttons[0].isRising) {
     //lose money
-    if(player.money>=foodPrices[option][0])
+    if (player.money >= foodPrices[option][0])
     {
-      player.money-=foodPrices[option][0];
+      player.money -= foodPrices[option][0];
       eatFood(foodPrices[option][1]);
       gainHappy(foodPrices[option][2]);
+      foodAnimations(option, player.age);
     }
     //display food
     //isFood = true >> eating animation? >> gain hunger/happy
-    
+
     goToPage(MainScreen);
   }
 
@@ -277,10 +282,12 @@ void handleFoodMenu() {
     goToPage(MainScreen);
   }
 }
-void handleGiftMenu(){
+void handleGiftMenu() {
   //display food options
   //display blablabla
-  int option = gameInputState.potentiometer/(PotentiometerMaxValue/6 + 1.0);
+  int option = gameInputState.potentiometer / (PotentiometerMaxValue / 6 + 1.0);
+  if (player.age == SENIOR && option == 5) //no more evolution at senior stage
+    option = 4;
   if (option != GlobalOption) {
     OrbitOledClear();
     GlobalOption = option;
@@ -288,14 +295,15 @@ void handleGiftMenu(){
   displayGifts(option, giftPrices[option][0], giftPrices[option][1], player.age);
   if (gameInputState.buttons[0].isRising) {
     //lose money
-    if(player.money>=giftPrices[option][0])
+    if (player.money >= giftPrices[option][0])
     {
-      player.money-=giftPrices[option][0];
+      player.money -= giftPrices[option][0];
       gainHappy(giftPrices[option][1]);
+      giftAnimations(option, player.age);
+      if (option == 5)
+        player.age++;
     }
-    //display food
-    //isFood = true >> eating animation? >> gain hunger/happy
-    
+
     goToPage(MainScreen);
   }
 
@@ -304,8 +312,42 @@ void handleGiftMenu(){
   }
 }
 void handleGameMenu() {
-  //game menu later
-  goToPage(MainScreen);
+  int x = 11, y1 = 5, y2 = 18, offset = 9; //where am I putting this
+  int option = gameInputState.potentiometer / (PotentiometerMaxValue / 2 + 1);
+  if (option != GlobalOption) {
+    OrbitOledClear();
+    GlobalOption = option;
+  }
+  OrbitOledMoveTo(x, y1);
+  OrbitOledDrawString("Dance");
+
+  //change x and y coordinates
+  OrbitOledMoveTo(x, y2);
+  OrbitOledDrawString("Jump");
+
+  //display the arrow
+  OrbitOledMoveTo(x - offset, (option == 0 ? y1 : y2));
+  OrbitOledDrawChar('>');
+
+  if (gameInputState.buttons[0].isRising && option == 0) {
+    initDanceGame();
+    goToPage(DanceGame);
+  }
+  else if (gameInputState.buttons[0].isRising && option == 1) {
+    goToPage(JumpGame);
+  }
+
+  if (gameInputState.buttons[1].isRising) {
+    goToPage(MainScreen);
+  }
+}
+
+void handleDanceGame() {
+  uint8_t reward = danceGame();
+  if (reward) {
+    player.money += reward;
+    gameUiPage = MainScreen;
+  }
 }
 
 const uint16_t petHeight = 16;
@@ -326,53 +368,53 @@ int hurdleIdx = 0;
 int level = 0;
 
 static void clearGameVar() {
-    //jumpGame
-    isJumping = false;
-    vy = 0;
-    y = petHeight;
-    for(int i = 0; i < numOfHurdles; i++)
-      hurdle[i] = 0;
-    hurdleIdx = 0;
-    slowcnt = 0;
+  //jumpGame
+  isJumping = false;
+  vy = 0;
+  y = petHeight;
+  for (int i = 0; i < numOfHurdles; i++)
+    hurdle[i] = 0;
+  hurdleIdx = 0;
+  slowcnt = 0;
 
-    //All
-    level = 1;
+  //All
+  level = 1;
 }
 
 static void handleJumpGame() {
 
-  if (slowcnt == slow-level) {
-  
+  if (slowcnt == slow - level) {
+
     OrbitOledClearBuffer();
     OrbitOledClear();
     slowcnt = 0;
-  
+
     //jumping
-    if (vy!=0){
-      y-=vy;
-      if (y==petHeight) {
+    if (vy != 0) {
+      y -= vy;
+      if (y == petHeight) {
         vy = 0;
         isJumping = false;
       }
-      if (y==maxHeight) vy = -vy;
+      if (y == maxHeight) vy = -vy;
     }
-  
+
     //hurdle generation
-    
-    if (rand()%30==0 && !hurdle[hurdleIdx]) {
+
+    if (rand() % 30 == 0 && !hurdle[hurdleIdx]) {
       hurdle[hurdleIdx] = 128;
-      hurdleIdx = (hurdleIdx+1)%numOfHurdles;
+      hurdleIdx = (hurdleIdx + 1) % numOfHurdles;
     }
 
     //draw pet
-    drawPet(player.age, xstart, y,0);
+    drawPet(player.age, xstart, y, 0);
 
     //collision detection
-    
-    for(int j = 0; j < numOfHurdles; j++) {
-      if (hurdle[j] >= xstart && xstart+petHeight >= hurdle[j] && y+petHeight >= screenSizeY-hurdleHeight) {
-        for(int i = 0; i < y+petHeight+hurdleHeight-screenSizeY; i++) {
-          if (checkPetBit(player.age, hurdle[j]-xstart, i)) {
+
+    for (int j = 0; j < numOfHurdles; j++) {
+      if (hurdle[j] >= xstart && xstart + petHeight >= hurdle[j] && y + petHeight >= screenSizeY - hurdleHeight) {
+        for (int i = 0; i < y + petHeight + hurdleHeight - screenSizeY; i++) {
+          if (checkPetBit(player.age, hurdle[j] - xstart, i)) {
             OrbitOledClear();
             OrbitOledMoveTo(5, 15);
             OrbitOledDrawString("Game Over");
@@ -384,10 +426,10 @@ static void handleJumpGame() {
     }
 
     //draw hurdle
-    for(int i = 0; i < numOfHurdles; i++) {
+    for (int i = 0; i < numOfHurdles; i++) {
       if (hurdle[i] != 0) {
         drawHurdle(hurdle[i], hurdleHeight);
-        hurdle[i]-= vx;
+        hurdle[i] -= vx;
       }
     }
   }
@@ -406,18 +448,18 @@ static void handleJumpGame() {
 }
 
 static void handleExitMenu(int game) {
-    //exit menu
+  //exit menu
 }
 
 //reads input on each tick, need one check per hardware element
 static void uiInputTick()
 {
   //switches
-  for(int i = 0; i < SwitchCount; ++i )
+  for (int i = 0; i < SwitchCount; ++i )
     gameInputState.switches[i] = digitalRead(Switches[i]);
 
   //buttons
-  for(int i = 0; i < ButtonCount; ++i )
+  for (int i = 0; i < ButtonCount; ++i )
   {
     // Only look for Rising Edge Signals.
     bool previousState = gameInputState.buttons[i].state;
@@ -428,39 +470,39 @@ static void uiInputTick()
   //potentiometer
   gameInputState.potentiometer = analogRead(Potentiometer);
 
-  
-//  player.hunger++;
-//  //Max integer: 2147483647
-//  Serial.println(player.hunger);
-//  struct PlayerStats
-//{
+
+  //  player.hunger++;
+  //  //Max integer: 2147483647
+  //  Serial.println(player.hunger);
+  //  struct PlayerStats
+  //{
 
 }
 
-void handleGameOver(){
+void handleGameOver() {
   gameOverGraphics();
   OrbitOledUpdate();
-  if (gameInputState.buttons[0].isRising||gameInputState.buttons[1].isRising) {
-    dead=false;
-    player={Egg,5,0,5,0,0,0,0};
+  if (gameInputState.buttons[0].isRising || gameInputState.buttons[1].isRising) {
+    dead = false;
+    player = {EGG, 5, 0, 5, 0, 0, 0, 0};
     goToPage(MainScreen);
   }
 }
 
-void handleGameStart(){
+void handleGameStart() {
   gameStartEgg();
-  for(int i=2000;i>1;i/=2)
+  for (int i = 2000; i > 1; i /= 2)
   {
-    digitalWrite(GREEN,LOW);
-    digitalWrite(RED,HIGH);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(RED, HIGH);
     delay(i);
-    digitalWrite(RED,LOW);
-    digitalWrite(GREEN,HIGH);
+    digitalWrite(RED, LOW);
+    digitalWrite(GREEN, HIGH);
     delay(i);
   }
   gameStartHatch();
   delay(1300);
-  player.age=Baby;
+  player.age = BABY;
   goToPage(MainScreen);
 }
 
@@ -469,36 +511,45 @@ void GameUITick()
 {
   uiInputTick();
 
-  switch(gameUiPage) {
+  switch (gameUiPage) {
 
-  case StartScreen:
-    handleGameStart();
-    break;
+    case StartScreen:
+      handleGameStart();
+      break;
 
-  case ShopMenu:
-    handleShopMenu();
-    break;
+    case ShopMenu:
+      handleShopMenu();
+      break;
 
-  case FoodMenu:
-    handleFoodMenu();
-    break;
+    case FoodMenu:
+      handleFoodMenu();
+      break;
 
-  case GiftMenu:
-    handleGiftMenu();
-    break;
+    case GiftMenu:
+      handleGiftMenu();
+      break;
 
-  case GameMenu:
-    handleGameMenu();
-    //handleJumpGame();
-    break;
+    case GameMenu:
+      handleGameMenu();
+      //handleJumpGame();
+      break;
 
-  case MainScreen:
-    handleMainScreen();
-    break;
+    case MainScreen:
+      handleMainScreen();
+      break;
 
-  case GameOver:
-    handleGameOver();
-    break;
+    case GameOver:
+      handleGameOver();
+      break;
+
+    case DanceGame:
+      handleDanceGame();
+      //handleMainScreen();
+      break;
+
+    case JumpGame:
+      handleJumpGame();
+      break;
   }
   OrbitOledUpdate();
 }
---
+++ Tamagotchi/zDanceGame.ino
@@ -0,0 +1,200 @@
+#include<stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+
+//0,12,22
+//Around 15 letters across the screen
+const uint8_t maxLevel = 20;
+uint8_t pattern[maxLevel] = {0};
+uint8_t danceLevel;
+bool lose;
+bool started;
+bool playerDancing;
+bool danceAnim;
+uint8_t age;
+uint8_t score;
+uint8_t playerActCnt;
+uint16_t shakeDanceCount;
+
+void initDanceGame() {
+  danceLevel = 0;
+  shakeDanceCount = 0;
+  lose = false;
+  started = false;
+  playerDancing = false;
+  danceAnim = false;
+  for(int i = 0; i < maxLevel; i++) {
+    pattern[i] = 0;
+  }
+  score = 0;
+  playerActCnt = 0;
+}
+
+
+
+uint8_t danceGameStart() {
+  started = true;
+  OrbitOledClear();
+  OrbitOledClearBuffer();
+  //Display instructions
+  OrbitOledMoveTo(0,0);
+  OrbitOledDrawString("Copy the pattern");
+  OrbitOledUpdate();
+  OrbitOledClearBuffer();
+  delay(1000);
+  OrbitOledMoveTo(0,12);
+  OrbitOledDrawString("A up | B down");
+  OrbitOledUpdate();
+  OrbitOledClearBuffer();
+  delay(1000);
+  OrbitOledMoveTo(0,12);
+  OrbitOledDrawString("shake board");
+  OrbitOledUpdate();
+  OrbitOledClearBuffer();
+  
+  pattern[0] = rand()%3;
+  delay(1000);
+  return 0;
+}
+
+
+uint8_t danceGame(){
+  if(!started)
+    return danceGameStart();
+  if(lose)
+    return danceGameEnd();
+  
+  if (!playerDancing && !lose) {
+    OrbitOledClear();
+    OrbitOledClearBuffer();
+    for (int i = -1; i <= level; i++){
+      drawPet(player.age, 56, 15, 0);
+      if (i == -1) {
+        OrbitOledMoveTo(25, 5);
+        OrbitOledDrawString("Ready...");
+      }
+      else if (pattern[i] == 0){
+        OrbitOledMoveTo(5,5);
+        OrbitOledDrawString("A");
+      }
+      else if (pattern[i] == 1){
+        OrbitOledMoveTo(40,5);
+        OrbitOledDrawString("B");
+      }
+      else if (pattern[i] == 2){
+        OrbitOledMoveTo(80,5);
+        OrbitOledDrawString("shake");
+      }
+
+      OrbitOledUpdate();
+      OrbitOledClearBuffer();
+      delay(500);
+      drawPet(player.age, 56, 15, 0);
+      OrbitOledUpdate();
+      delay(500);
+    }
+    playerDancing = true;
+  }
+
+
+  if (playerDancing) {
+    OrbitOledClearBuffer();
+    drawPet(player.age, 56, 15, 0);
+    OrbitOledMoveTo(40, 5);
+    if (playerActCnt==0)
+      OrbitOledDrawString("Go!");
+    else
+      OrbitOledDrawString("Then...");
+    if (playerDancing && gameInputState.buttons[0].isRising) {
+      animateDance(0);
+      if (pattern[playerActCnt]!=0) {
+          lose = true;
+          playerDancing = false;
+      }
+      playerActCnt++;
+    }
+    else if (playerDancing && gameInputState.buttons[1].isRising) {
+      animateDance(1);
+      if (pattern[playerActCnt]!=1) {
+          lose = true;
+          playerDancing = false;
+      }
+      playerActCnt++;
+    }
+    else if (shakeDanceCount == 50) {
+      shakeDanceCount = 0;
+      animateDance(2);
+      if (pattern[playerActCnt]!=2) {
+          lose = true;
+          playerDancing = false;
+      }
+      playerActCnt++;
+    }
+    else if (ShakeIsShaking()) {
+      shakeDanceCount++;
+    }
+
+    if (!lose && playerActCnt > level) {
+        OrbitOledClearBuffer();
+        drawPet(player.age, 56, 15, 0);
+        level++;
+        playerActCnt = 0;
+        playerDancing = false;
+        pattern[level] = rand()%3;
+        //less likely to get same pattern twice in a row
+        if (pattern[level] == pattern[level-1] && rand()%2 == 0)
+          pattern[level] = rand()%3;
+        if (level > maxLevel)
+            danceGameEnd();
+    }
+  }
+  return 0;
+}
+
+uint8_t danceGameEnd() {
+  OrbitOledClear();
+  OrbitOledClearBuffer();
+  if (lose) {
+    OrbitOledMoveTo(5, 15);
+    OrbitOledDrawString("Game Over");
+    OrbitOledUpdate();
+    delay(2000);
+    OrbitOledClear();
+    OrbitOledClearBuffer();
+    return level*5;
+  }
+  else {
+    OrbitOledMoveTo(5, 15);
+    OrbitOledDrawString("You Win!");
+    delay(2000);
+    return level*20;
+  }
+}
+
+void animateDance(uint8_t dpattern) {
+  OrbitOledClearBuffer();
+  switch(dpattern) {
+  case 0:
+    OrbitOledMoveTo(5,5);
+    OrbitOledDrawString("A");
+    drawPet(player.age, 56, 15, 1);
+    OrbitOledUpdate();
+    delay(500);
+    break;
+  case 1:
+    OrbitOledMoveTo(40,5);
+    OrbitOledDrawString("B");
+    drawPet(player.age, 56, 15, 1);
+    OrbitOledUpdate();
+    delay(500);
+    break;
+  case 2:
+    OrbitOledMoveTo(80,5);
+    OrbitOledDrawString("shake");
+    drawPet(player.age, 56, 15, 1);
+    OrbitOledUpdate();
+    delay(500);
+    break;
+  }
+}
+
