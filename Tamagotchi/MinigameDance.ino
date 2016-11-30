c3a7aacbf6511a62e49ce673d802ea3a059be7c8
s38qiu <s38qiu@06e43772-ae1b-4c0d-9892-c0dabbf93334>
2016-11-30 11:51:55 +0000

    Moved Jump minigame into its own file. Reorganized and clarified much of the code.

--- Tamagotchi/MinigameDance.ino
+++ Tamagotchi/MinigameDance.ino
@@ -4,10 +4,12 @@
 //Variables for Dance Game
 const uint8_t dance_maxLevel = 15;
 
+
 bool dance_lose;
 bool dance_start;
 bool dance_play;
 bool dance_animate;
+bool dance_update;
 uint8_t dance_level;
 uint8_t dance_actionCount;
 uint16_t dance_shakeCount;
@@ -18,6 +20,7 @@ void initDanceGame() {
   dance_lose = false;
   dance_start = false;
   dance_play = false;
+  dance_update = false;
   dance_animate = false;
   dance_level = 0;
   dance_shakeCount = 0;
@@ -31,7 +34,7 @@ uint8_t danceGameStart() {
   dance_start = true;
     
   //Display instructions
-  drawString("Copy the Pattern", 0, 0);
+  drawString("Copy the Pattern", 0, 12);
   updateScreen();
   delay(2000);
   drawString("A up | B down", 0, 12);
@@ -42,7 +45,7 @@ uint8_t danceGameStart() {
   delay(2000);
     
   dance_pattern[0] = rand()%3;
-  return 0;
+  return 1;
 }
 
 uint8_t danceGameEnd() {
@@ -50,47 +53,13 @@ uint8_t danceGameEnd() {
     drawString("Game Over", 5, 15);
     updateScreen();
     delay(2000);
-    return dance_level*5;
+    return dance_level;
   }
   else {
     drawString("You Win!", 5, 15);
     updateScreen();
     delay(2000);
-    return dance_level*20;
-  }
-}
-
-
-
-void danceReceiveInput() {
-  if (gameInputState.buttons[0].isRising) {
-    animateDance(0);
-    if (dance_pattern[dance_actionCount]!=0) {
-        dance_lose = true;
-        dance_play = false;
-    }
-    dance_actionCount++;
-  }
-  else if (gameInputState.buttons[1].isRising) {
-    animateDance(1);
-    if (dance_pattern[dance_actionCount]!=1) {
-        dance_lose = true;
-        dance_play = false;
-    }
-    dance_actionCount++;
-  }
-  //check shaking (must shake for long enough to ensure the user is shaking)
-  else if (ShakeIsShaking()) {
-    dance_shakeCount++;
-  }
-  else if (dance_shakeCount == 50) {
-    dance_shakeCount = 0;
-    animateDance(2);
-    if (dance_pattern[dance_actionCount]!=2) {
-        dance_lose = true;
-        dance_play = false;
-    }
-    dance_actionCount++;
+    return dance_level+10;
   }
 }
 
@@ -115,7 +84,7 @@ uint8_t danceLevelUp() {
   //less likely to get same pattern twice in a row (1/9)
   if (dance_pattern[dance_level] == dance_pattern[dance_level-1])
     dance_pattern[dance_level] = rand()%3;
-  return 0;
+  return 1;
 }
 
 uint8_t danceGame() {
@@ -130,7 +99,6 @@ uint8_t danceGame() {
   if (!dance_play && !dance_lose) {
     //show the dance steps
     for (int i = -1; i <= dance_level; i++){
-      drawPet(player.age, 56, 15, 0);
       if (i == -1) 
         drawString("Ready...", 25, 5);
       else
@@ -152,9 +120,39 @@ uint8_t danceGame() {
         drawString("Go!", 40, 5);
     else
       drawString("then...", 40, 5);
-    updateScreen();
+    OrbitOledUpdate();
     //take user input
-    danceReceiveInput();
+    if (gameInputState.buttons[0].isRising) {
+      OrbitOledClearBuffer();
+      animateDance(0);
+      if (dance_pattern[dance_actionCount]!=0) {
+          dance_lose = true;
+          dance_play = false;
+      }
+      dance_actionCount++;
+    }
+    else if (gameInputState.buttons[1].isRising) {
+      OrbitOledClearBuffer();
+      animateDance(1);
+      if (dance_pattern[dance_actionCount]!=1) {
+          dance_lose = true;
+          dance_play = false;
+      }
+      dance_actionCount++;
+    }
+    else if (dance_shakeCount == 50) {
+      dance_shakeCount = 0;
+      OrbitOledClearBuffer();
+      animateDance(2);
+      if (dance_pattern[dance_actionCount]!=2) {
+          dance_lose = true;
+          dance_play = false;
+      }
+      dance_actionCount++;
+    }
+    else if (ShakeIsShaking()) {
+      dance_shakeCount++;
+    }
 
     //level up
     if (!dance_lose && dance_actionCount > dance_level) {
@@ -162,5 +160,5 @@ uint8_t danceGame() {
     }      
 
   }
-  return 0;
+  return 1;
 }
